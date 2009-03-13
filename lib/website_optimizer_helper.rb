e5561b83a7ebc4cf2da6364f5251959996485d73
Adam Patten <adampatten@kidzui.com>
2009-03-13 11:04:03 -0700

    Fix for strict doc types

--- lib/website_optimizer_helper.rb
+++ lib/website_optimizer_helper.rb
@@ -43,7 +43,7 @@ module WebsiteOptimizerHelper
   def multivariate_section_script(description, &block)
     if block_given?
       content = capture(&block)
-      concat(content_tag('script', "utmx_section(\"#{description}\")"), block.binding)
+      concat(content_tag('script', "utmx_section(\"#{description}\")", :type => 'text/javascript'), block.binding)
       concat(content, block.binding)
       concat("</noscript>", block.binding)
     else
@@ -56,12 +56,12 @@ module WebsiteOptimizerHelper
   end
 
   def multivariate_controll_script(tracker_id)
-    content_for(:head) { content_tag('script', "function utmx_section(){}function utmx(){}(function(){var k='#{tracker_id}',d=document,l=d.location,c=d.cookie;function f(n){if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.indexOf(';',i);return c.substring(i+n.length+1,j<0?c.length:j)}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;d.write('<sc'+'ript src=\"'+'http'+(l.protocol=='https:'?'s://ssl':'://www')+'.google-analytics.com'+'/siteopt.js?v=1&utmxkey='+k+'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='+new Date().valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+'\" type=\"text/javascript\" charset=\"utf-8\"></sc'+'ript>')})();") }
+    content_for(:head) { content_tag('script', "function utmx_section(){}function utmx(){}(function(){var k='#{tracker_id}',d=document,l=d.location,c=d.cookie;function f(n){if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.indexOf(';',i);return c.substring(i+n.length+1,j<0?c.length:j)}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;d.write('<sc'+'ript src=\"'+'http'+(l.protocol=='https:'?'s://ssl':'://www')+'.google-analytics.com'+'/siteopt.js?v=1&utmxkey='+k+'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='+new Date().valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+'\" type=\"text/javascript\" charset=\"utf-8\"></sc'+'ript>')})();", :type => 'text/javascript') }
   end
 
   def ab_controll_script(tracker_id)
     multivariate_controll_script(tracker_id)
-    content_for(:head) { content_tag('script', "utmx(\"url\",'A/B');") }
+    content_for(:head) { content_tag('script', "utmx(\"url\",'A/B');", :type => 'text/javascript') }
   end
 
   def ab_tracking_script(tracker_id)
